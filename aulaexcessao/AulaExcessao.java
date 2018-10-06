1408b5d58110c1be76ccf7b8815bd353c39c78c5
ronansr <42754743+ronansr@users.noreply.github.com>
2018-10-06 18:11:13 -0300

    Add files via upload

---
+++ aulaexcessao/AulaExcessao.java
@@ -0,0 +1,10 @@
+package aulaexcessao;
+public class AulaExcessao {
+    public static void main(String[] args) {
+          System.out.println("Divisão: ");
+          float res = Calculador.resultado(5, 0);
+          System.out.println("Resultado: " + res);
+          Calculador.corre();
+    }
+    
+}
---
+++ aulaexcessao/Calculador.java
@@ -0,0 +1,35 @@
+package aulaexcessao;
+public class Calculador {
+    
+    public static float resultado(int a, int b){
+        float x = 0;
+        try{
+            x = a/b;
+        }catch (ArithmeticException e){
+            System.out.println("Erro! Divisão por zero!");
+        }
+        return x;
+    }
+    public static void corre(){
+        int [] vet = new int[2];
+
+        for(int i = 0; i < 3; i++){
+            try{
+                vet[i] += i;
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        
+        for(int i = 0; i < 3; i++){
+            try{
+                System.out.println("Divisão: " + Calculador.resultado(vet[i], 0));
+            }catch(Exception e){
+                
+            }
+        }
+        
+    }
+
+}
+
